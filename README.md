4432d735d911b3b275b1b6318a0c91e384afe545
Uri Kalish <uri.kalish@hpe.com>
2016-04-18 19:57:56 +0300

    readme

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ GitNagPush by Uri Kalish.
 This batch file will try to pull, rebase, and push until it succeeds, while sleeping for X seconds between failed attempts.
 Ideal for pushing to soon-to-melt frozen branches while enjoying sushi for lunch.
 
-**Before the first usage, please configure the PERSONALIZED SETTINGS section in the file.**
+**Before the first use, please configure the PERSONALIZED SETTINGS section in the file.**
--- git-nag-push.bat
+++ git-nag-push.bat
@@ -1,7 +1,7 @@
 REM GitNagPush by Uri Kalish
 REM This batch file will try to pull, rebase, and push until it succeeds, while sleeping for X seconds between failed attempts.
 REM Ideal for pushing to soon-to-melt frozen branches while enjoying sushi for lunch.
-REM Before the first usage, please configure the PERSONALIZED SETTINGS section below:
+REM Before the first use, please configure the PERSONALIZED SETTINGS section below:
 
 setlocal
 

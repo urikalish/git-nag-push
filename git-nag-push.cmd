0b0f63425053af35bdea7bf39f253a7a303b2296
Uri Kalish <uri.kalish@hpe.com>
2018-01-18 22:46:56 +0200

    echo

--- git-nag-push.cmd
+++ git-nag-push.cmd
@@ -1,32 +1,32 @@
+@echo off
+setlocal
 REM GitNagPush by Uri Kalish
 REM This cmd file will try to pull, rebase, and push until it succeeds, while sleeping for X minutes between failed attempts.
 REM Ideal for pushing to soon-to-melt frozen branches while enjoying sushi for lunch.
 REM Before the first use, please configure the PERSONALIZED SETTINGS section below:
-
-setlocal
 
 REM ********** PERSONALIZED SETTINGS - START
 set localGitRepositoryPath=C:\QC\Views\Git\mqm
 set minutesToWaitBetweenAttempts=5
 REM ********** PERSONALIZED SETTINGS - END
 
-set codeVersion=1.0.5
+set codeVersion=1.0.6
 set echoPrefix=GitNagPush:
 set counter=0;
-@echo off
 color 0f
 cls
-@echo -------------------------------
+@echo .
+@echo --------------------------------------------------------------------------------
 @echo GitNagPush v%codeVersion% by Uri Kalish
-@echo -------------------------------
+@echo --------------------------------------------------------------------------------
 echo %echoPrefix% Changing directory to %localGitRepositoryPath%...
 cd %localGitRepositoryPath%
-echo %echoPrefix% CD OK
+echo %echoPrefix% Changing directory - OK
 echo %echoPrefix% Minutes between attempts: %minutesToWaitBetweenAttempts%
 set /a secondsToWaitBetweenAttempts=minutesToWaitBetweenAttempts*60
 
 :loopstart
-@echo -------------------------------
+@echo --------------------------------------------------------------------------------
 set /a counter=counter+1
 echo %echoPrefix% Attempt #%counter%
 
